57d950b630202697a001a49f55eebc95920cb712
EricTW-91 <amperic05@gmail.com>
2021-06-28 14:10:36 -0700

    import the Link to detail pages from random card

--- src/components/home/PropertyCard.jsx
+++ src/components/home/PropertyCard.jsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const PropertyCard = (props) => {
     return (
-        <img
-            src={props.data.optimizedThumbUrls.srpDesktop}
-            style={{width: '30vw'}}
-        />
+        <Link to={`/detail/${props.data.id}`}>
+            <img
+                src={props.data.optimizedThumbUrls.srpDesktop}
+                style={{width: '30vw'}}
+            />
+
+        </Link>
      );
 }
  
